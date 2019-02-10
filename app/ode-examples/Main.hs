37deb47ef291d27c88a7658cca6091e064584fce
Jonathan Merritt <j.s.merritt@gmail.com>
2019-02-10 22:04:17 +1100

    Switch to Chart
    - Revert back to an earlier LTS
    - Push vector-sized to the version in the *current* LTS
    - Include Chart, Chart-diagrams and SVGFonts in extra-deps

--- app/ode-examples/Main.hs
+++ app/ode-examples/Main.hs
@@ -2,8 +2,8 @@
 {-# LANGUAGE DataKinds #-}
 module Main where
 
-import qualified Graphics.Gnuplot.Simple as Gnuplot
-import qualified Graphics.Gnuplot.Terminal.SVG as SVG
+import qualified Graphics.Rendering.Chart.Easy as Chart
+import qualified Graphics.Rendering.Chart.Backend.Diagrams as ChartDiagrams
 import qualified Data.Maybe as Maybe
 import qualified Data.Vector.Unboxed.Sized as USized
 
@@ -17,16 +17,17 @@ shm :: IO ()
 shm = do
   let
     state0 = Maybe.fromJust $ USized.fromList [ 1.0, 0.0 ]
-    integResult = ODE.odeFixedSteps ODE.eulerStep [0.0, 0.1 .. 5.0] state0 shmfn
+    integResult = ODE.odeFixedSteps ODE.eulerStep [0.0, 0.01 .. 15.0] state0 shmfn
     plotValues = fmap (\(t, v) -> (t, USized.index v 0)) integResult
   putStrLn "Hello World"
-  Gnuplot.plotList [Gnuplot.terminal (SVG.cons "test.svg")] plotValues
+  ChartDiagrams.toFile Chart.def "test.svg" $ do
+    Chart.plot (Chart.line "integrated" [plotValues])
 
 shmfn :: (Double, USized.Vector 2 Double) -> USized.Vector 2 Double
 shmfn (_, s) = Maybe.fromJust $ USized.fromList [ v, -k * x / m ]
   where
     m = 1.0
-    k = 1.0
+    k = 3.0
     x = USized.index s 0
     v = USized.index s 1
 
