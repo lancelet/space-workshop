0246d2b37172253603562a07fbe451606506772c
Jonathan Merritt <j.s.merritt@gmail.com>
2019-02-18 17:47:54 +1100

    Add RK4

--- src/ODE/FixedStepV.hs
+++ src/ODE/FixedStepV.hs
@@ -1,15 +1,45 @@
 {-# LANGUAGE TypeFamilies #-}
+{-# LANGUAGE ScopedTypeVariables #-}
 module ODE.FixedStepV where
 
 import Data.AffineSpace (AffineSpace, Diff, (.+^))
 import Data.VectorSpace (VectorSpace, Scalar, (*^))
 
 eulerStep
-  :: ( AffineSpace s
-     , d ~ Diff s, VectorSpace d
-     , a ~ Scalar d, Num a )
-  => a              -- ^ Step size.
-  -> (a, s)         -- ^ Integration value and state before step.
-  -> ((a, s) -> d)  -- ^ Gradient function.
-  -> (a, s)         -- ^ Final state.
+  :: ( AffineSpace as
+     , vs ~ Diff as, VectorSpace vs
+     , a ~ Scalar vs, Num a )
+  => a                -- ^ Step size.
+  -> (a, as)          -- ^ Integration value and state before step.
+  -> ((a, as) -> vs)  -- ^ Gradient function.
+  -> (a, as)          -- ^ Final state.
 eulerStep h q@(x, y) f = (x + h, y .+^ h *^ f q)
+
+
+rk4Step
+  :: forall as vs a.
+     ( AffineSpace as
+     , vs ~ Diff as, VectorSpace vs
+     , a ~ Scalar vs, Fractional a )
+  => a                -- ^ Step size.
+  -> (a, as)          -- ^ Integration value and state before step.
+  -> ((a, as) -> vs)  -- ^ Gradient function.
+  -> (a, as)          -- ^ Final state.
+rk4Step h (x, y) f =
+  let
+    sixth, third :: a
+    sixth = (1.0 / 6.0)
+    third = (1.0 / 3.0)
+  in
+    let
+      k1, k2, k3, k4 :: vs
+      k1 = h *^ f (x, y)
+      k2 = h *^ f (x + 0.5 * h, y .+^ 0.5 *^ k1)
+      k3 = h *^ f (x + 0.5 * h, y .+^ 0.5 *^ k2)
+      k4 = h *^ f (x + h, y .+^ k3)
+    in
+      ( x + h
+      , y .+^ sixth *^ k1
+          .+^ third *^ k2
+          .+^ third *^ k3
+          .+^ sixth *^ k4 )
