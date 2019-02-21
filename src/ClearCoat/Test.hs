bb4d75fb21cb4c6649ef55ed8aab237ec1103e80
Jonathan Merritt <j.s.merritt@gmail.com>
2019-02-21 21:26:55 +1100

    SDL window creation

--- src/ClearCoat/Test.hs
+++ src/ClearCoat/Test.hs
@@ -1,5 +1,39 @@
+{-# LANGUAGE OverloadedStrings #-}
 module ClearCoat.Test where
+
+import Control.Monad (unless)
+import SDL(($=))
+import Linear (V2(V2))
+import qualified SDL
+import qualified Graphics.Rendering.OpenGL as GL
 
 test :: IO ()
 test = do
   putStrLn "Hello World"
+
+  SDL.initialize [SDL.InitVideo]
+  SDL.HintRenderScaleQuality $= SDL.ScaleLinear
+
+  window <- SDL.createWindow
+              "ClearCoat Test / Dev"
+              SDL.defaultWindow
+              { SDL.windowInitialSize = V2 800 600
+              , SDL.windowHighDPI = True
+              , SDL.windowOpenGL = Just SDL.defaultOpenGL
+              , SDL.windowResizable = True
+              }
+  SDL.showWindow window
+  _ <- SDL.glCreateContext window
+
+  let loop = do
+        events <- SDL.pollEvents
+        let quit = elem SDL.QuitEvent $ map SDL.eventPayload events
+
+        GL.clear [GL.ColorBuffer]
+        SDL.glSwapWindow window
+
+        unless quit loop
+  loop
+
+  SDL.destroyWindow window
+  SDL.quit
