de1cbdbefd31a58ffb5d2720b29b1747e23723f1
Jonathan Merritt <j.s.merritt@gmail.com>
2019-04-30 15:21:50 +1000

    SHM example plot

--- app/tex-plots/Main.hs
+++ app/tex-plots/Main.hs
@@ -5,14 +5,39 @@ Description : Plotting diagrams for the notes.
 {-# LANGUAGE OverloadedStrings #-}
 module Main where
 
+import           Control.Monad        (forM_)
+import           System.IO            (hFlush, stdout)
+
 import qualified Examples.ODEExamples as ODEExamples
-import qualified Plot as Plot
+import qualified Plot                 as Plot
+
 
 main :: IO ()
 main = do
+  let plots
+        = [ (ODEExamples.plotEulerDoubleExpDecay, "euler-double-exp-decay")
+          , (ODEExamples.plotEulerSHM, "euler-shm")
+          ]
+
   putStrLn "Plotting diagrams for the notes."
-  ODEExamples.plotEulerDoubleExpDecay (Plot.PGF "./notes/fig/euler-double-exp-decay.tex")
+  forM_ plots $ \(plotFn, name) -> plotPGF plotFn name
   putStrLn "Done plotting diagrams for the notes."
 
 
-  
+plotPGF :: (Plot.Output -> IO ()) -> FilePath -> IO ()
+plotPGF plotFn name = do
+  let
+    fullName = "./notes/fig/" ++ name ++ ".tex"
+
+    fgReset = "\x1b[0m"
+    fgGreen = "\x1b[32m"
+    fgBlue  = "\x1b[34m"
+
+    inBlue x = fgBlue ++ x ++ fgReset
+    inGreen x = fgGreen ++ x ++ fgReset
+
+  putStr $ "  -> Plotting " ++ (inBlue fullName) ++ "..."
+  hFlush stdout
+  plotFn (Plot.PGF fullName)
+  putStr $ " " ++ (inGreen "DONE!") ++ "\n"
+  hFlush stdout
