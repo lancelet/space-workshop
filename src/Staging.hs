c04003c9cc64e77dc69778f8b8dccbfc3cf5b816
Jonathan Merritt <j.s.merritt@gmail.com>
2019-05-04 12:50:33 +1000

    Finish off staging exercises and plot

--- src/Staging.hs
+++ src/Staging.hs
@@ -2,15 +2,18 @@
 Module      : Staging
 Description : Simulating staging of rockets.
 -}
-{-# LANGUAGE TypeOperators #-}
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE TypeOperators     #-}
 {-# OPTIONS_GHC -Wno-unused-imports #-} -- re-enable after completing solutions
 module Staging where
 
 import           Data.LinearMap     ((:-*), linear)
 import           Data.List.NonEmpty (NonEmpty)
+import qualified Data.List.NonEmpty as NonEmpty
 import           Data.VectorSpace   ((^*))
 
 import qualified ODE
+import qualified Plot
 import qualified Solutions.Staging
 import qualified Staging.Types      as T
 import           Todo               (FallbackSolution (FallbackSolution), todo)
@@ -44,19 +47,78 @@ equationOfMotion {- mDot isp mDry mRemaining (_, state) -}
   -}
 
 
+-- | Burn a stage of a rocket (can be the first or second stage).
 burnStage
   :: Double                      -- ^ Initial propellant mass (kg).
   -> Double                      -- ^ Dry mass of the current stage (kg).
   -> Double                      -- ^ Total mass of remaining mass of stages (kg).
   -> Int                         -- ^ Number of integration steps.
+  -> Double                      -- ^ Initial time (s).
   -> T.State                     -- ^ Initial state.
   -> NonEmpty (Double, T.State)  -- ^ List of time and state tuples.
-burnStage = undefined
-  
+burnStage {- mp0 mDry mRemaining nSteps time0 state0 -}
+  = todo (FallbackSolution Solutions.Staging.burnStage)
+  {-
+  where
+    -- Duration of the burn (s).
+    burnDuration :: Double
+    burnDuration = undefined
 
-burnSingleStage :: Int -> NonEmpty T.State
-burnSingleStage = undefined
+    -- Evaluation times (s).
+    times :: NonEmpty Double
+    times = NonEmpty.fromList
+            $ ODE.linspace nSteps time0 (time0 + burnDuration)
+
+    -- Gradient function for ODE solver.
+    gradFn :: (Double, T.State) -> Double :-* T.DState
+    gradFn = undefined
+
+    -- Mass flow rate (kg / s).
+    mDot :: Double
+    mDot = 290.0
+
+    -- Specific impulse (s).
+    isp :: Double
+    isp = 300.0
+   -}
 
 
-burnTwoStage :: Int -> NonEmpty T.State
-burnTwoStage = undefined
+-- | Burn the single stage rocket only.
+burnSingleStage
+  :: Int                          -- ^ Number of integration steps per stage.
+  -> NonEmpty (Double, T.State)   -- ^ Time and state list.
+burnSingleStage {- nSteps = burnStage mp0 mDry 0.0 nSteps state0 -}
+  = todo (FallbackSolution Solutions.Staging.burnSingleStage)
+
+
+-- | Burn the two stage rocket only.
+burnTwoStage
+  :: Int                         -- ^ Number of integration steps per stage.
+  -> NonEmpty (Double, T.State)  -- ^ Time and state list.
+burnTwoStage {- nSteps = stage1 <> stage2 -}
+  = todo (FallbackSolution Solutions.Staging.burnTwoStage)
+
+
+-- | Plot the comparison of velocity history for the staging options.
+plotVelocityComparison :: Plot.Output -> IO ()
+plotVelocityComparison output =
+  let
+    nSteps :: Int
+    nSteps = 100
+
+    extractVel :: (Double, T.State) -> (Double, Double)
+    extractVel (t, state) = (t, T.velocity state)
+
+    v1Stage :: [(Double, Double)]
+    v1Stage = NonEmpty.toList (extractVel <$> burnSingleStage nSteps)
+
+    v2Stage :: [(Double, Double)]
+    v2Stage = NonEmpty.toList (extractVel <$> burnTwoStage nSteps)
+  in
+    Plot.xyChart
+      output
+      "Single Stage vs Two Stage Velocity Comparison"
+      "Time (s)"
+      "Velocity (m/s)"
+      [ Plot.Line "Single Stage" v1Stage
+      , Plot.Line "Two Stage" v2Stage ]
