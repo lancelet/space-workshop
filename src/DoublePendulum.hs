1ce53ef499b8d2bb9e8798be42b1547d5c8dede7
Jonathan Merritt <j.s.merritt@gmail.com>
2019-05-10 17:47:10 +1000

    Better rendering

--- src/DoublePendulum.hs
+++ src/DoublePendulum.hs
@@ -1,8 +1,9 @@
 {-|
+Module      : DoublePendulum
+Description : Simulation of a double pendulum.
 
 Equations from here: https://en.wikipedia.org/wiki/Double_pendulum
 -}
-
 {-# LANGUAGE DeriveAnyClass   #-}
 {-# LANGUAGE DeriveGeneric    #-}
 {-# LANGUAGE NegativeLiterals #-}
@@ -15,14 +16,13 @@ import           Codec.Picture                       (Image,
                                                       PixelRGBF)
 import qualified Codec.Picture                       as P
 import qualified Codec.Picture.Types                 as P
-import           Control.Parallel.Strategies         (parMap)
+import           Control.Parallel.Strategies         (parMap, rpar)
 import           Data.AdditiveGroup                  (AdditiveGroup)
 import           Data.AffineSpace                    (AffineSpace, Diff, (.+^),
                                                       (.-.))
 import           Data.LinearMap                      (linear)
+import           Data.List                           (inits)
 import qualified Data.List.NonEmpty                  as NonEmpty
-import           Data.Vector                         (Vector)
-import qualified Data.Vector                         as V
 import           Data.VectorSpace                    (VectorSpace, (*^))
 import           GHC.Generics                        (Generic)
 import           Graphics.Rasterific                 (V2 (V2))
@@ -33,6 +33,7 @@ import qualified Graphics.Rasterific.Transformations as R
 
 import qualified ODE                                 as ODE
 
+
 test :: IO ()
 test = do
   let
@@ -42,6 +43,9 @@ test = do
          , segColor = PixelRGBA8 0 0x86 0xc1 255
          , segPixelWidth = 6
          , padFraction = 1.1
+         , traceColor = PixelRGBA8 0xc1 0xc1 0xc1 255
+         , tracePixelWidth = Just 1
+         , traceSubframes = Just 50
          }
     params = Params
              { segMass   = 3.0
@@ -54,7 +58,7 @@ test = do
                    , momentum2 = 0
                    }
 
-  doublePendulumSim rp params state0 10.0 4 10 "test.gif"
+  doublePendulumSim rp params state0 2.0 4 10 "test.gif"
 
 
 doublePendulumSim
@@ -74,16 +78,16 @@ doublePendulumSim rp p state0 tf period subframes filePath = do
 
     gradFn (_, state) = linear $ \dt -> dt *^ grad p state
     states = ODE.integrate ODE.rk4Step state0 times gradFn
-    statesVec = V.fromList $ NonEmpty.toList states
+    statesList = NonEmpty.toList states
 
-    subFrameImages = renderFrame rp p . snd <$> statesVec
-    frameImages = averageImages <$> takeGroupN subframes subFrameImages
+    subFrameImages = parMap rpar (renderFrame rp p . fmap snd) (tail $ inits statesList)
+    frameImages = parMap rpar averageImages (takeGroupN subframes subFrameImages)
     rgb8Images = P.convertRGB8 . P.ImageRGBF <$> frameImages
 
     result = P.writeGifAnimation filePath
                                  period
                                  P.LoopingForever
-                                 (V.toList rgb8Images)
+                                 rgb8Images
   case result of
     Left err -> do
       putStrLn "An error occurred while writing the DoublePendulum gif:"
@@ -109,41 +113,23 @@ grad p s =
     }
 
 
-{-
-renderGif
-  :: RenderParams           -- ^ Render parameters
-  -> Params                 -- ^ Double-pendulum parameters
-  -> Int                    -- ^ Frame period (1/100 s)
-  -> Vector (Vector State)  -- ^ Vector containing multiple states for each frame
-  -> FilePath               -- ^ Output file path
-  -> IO ()                  -- ^ IO action
-renderGif rp p period ss filePath = do
-  let
-    baseImages = fmap (renderFrame rp p) <$> ss
-    images = P.convertRGB8 . P.ImageRGBF <$> averageImages <$> baseImages
-    result = P.writeGifAnimation filePath
-                                 period
-                                 P.LoopingForever
-                                 (V.toList images)
-  case result of
-    Left err -> do
-      putStrLn "An error occurred while writing the DoublePendulum gif:"
-      putStrLn err
-    Right action -> action
--}
-
-
-takeGroupN :: Int -> Vector a -> Vector (Vector a)
+takeGroupN :: Int -> [a] -> [[a]]
 takeGroupN n v =
   let
-    (cur, remainder) = V.splitAt n v
+    (cur, remainder) = splitAt n v
   in
-    if V.length remainder == 0
-    then V.singleton cur
-    else V.fromList (cur : V.toList (takeGroupN n remainder))
+    case remainder of
+      [] -> [cur]
+      _  -> cur : takeGroupN n remainder
 
 
-averageImages :: Vector (Image PixelRGBF) -> Image PixelRGBF
+seqPairs :: [a] -> [(a, a)]
+seqPairs (a : b : []) = [(a, b)]
+seqPairs (a : b : xs) = (a, b) : seqPairs (b : xs)
+seqPairs _ = []
+
+
+averageImages :: [Image PixelRGBF] -> Image PixelRGBF
 averageImages images =
   let
     imgSum :: Image PixelRGBF
@@ -153,7 +139,7 @@ averageImages images =
     sumPair i1 i2 = P.zipPixelComponent3 (\c1 c2 _ -> c1 + c2) i1 i2 i2
 
     nF :: Float
-    nF = fromIntegral (V.length images)
+    nF = fromIntegral (length images)
 
     avg :: Image PixelRGBF
     avg = P.pixelMap (P.colorMap (\c -> c / nF)) imgSum
@@ -164,46 +150,55 @@ averageImages images =
 renderFrame
   :: RenderParams
   -> Params
-  -> State
+  -> [State]
   -> Image PixelRGBF
-renderFrame rp p s =
+renderFrame rp p states =
   let
+    s = last states
+
     scaleFactor = fromIntegral(imgSize rp) / (4*padFraction rp*segLength p)
-    center = --fromIntegral(imgSize rp) / 2
-      (2*padFraction rp*segLength p)
+    center = 2*padFraction rp*segLength p
     xform = R.scale scaleFactor scaleFactor
             <> R.translate (V2 center center)
             <> R.scale 1 -1
 
-    drawSeg p1 p2 = R.stroke (segPixelWidth rp)
-                             R.JoinRound
-                             (R.CapRound, R.CapRound)
-                             (R.line
-                                (R.applyTransformation xform p1)
-                                (R.applyTransformation xform p2))
-
     o  = V2 0 0
-    e1 = o  RL.^+^ V2 (segLength p*sin(angle1 s)) (-segLength p*cos(angle1 s))
-    e2 = e1 RL.^+^ V2 (segLength p*sin(angle2 s)) (-segLength p*cos(angle2 s))
+    e1 s' = o     RL.^+^ V2 (segLength p*sin(angle1 s')) (-segLength p*cos(angle1 s'))
+    e2 s' = e1 s' RL.^+^ V2 (segLength p*sin(angle2 s')) (-segLength p*cos(angle2 s'))
 
-    seg1 = drawSeg o  e1
-    seg2 = drawSeg e1 e2
+    seg1 = R.line 0      (e1 s)
+    seg2 = R.line (e1 s) (e2 s)
+    segs = R.transform (R.applyTransformation xform) (seg1 <> seg2)
+
+    trace = mconcat
+          $ (\(s1, s2) -> R.line (e2 s1) (e2 s2)) <$> seqPairs
+          (maybe states (\n -> reverse $ take n $ reverse states) (traceSubframes rp))
+    (traces, traceW) =
+      case tracePixelWidth rp of
+        Just w  -> (R.transform (R.applyTransformation xform) trace, w)
+        Nothing -> (mempty, 0)
+        
   in
     P.promoteImage
     $ P.dropAlphaLayer
     $ R.renderDrawing (imgSize rp) (imgSize rp) (bgColor rp) $
-      R.withTexture (R.uniformTexture (segColor rp)) $ do
-        seg1
-        seg2
+      (R.withTexture (R.uniformTexture (traceColor rp)) $
+        R.stroke traceW R.JoinRound (R.CapRound, R.CapRound) traces)
+      <>
+      (R.withTexture (R.uniformTexture (segColor rp)) $
+        R.stroke (segPixelWidth rp) R.JoinRound (R.CapRound, R.CapRound) segs)
 
 
 data RenderParams
   = RenderParams
-    { imgSize       :: Int
-    , bgColor       :: PixelRGBA8
-    , segColor      :: PixelRGBA8
-    , segPixelWidth :: Float
-    , padFraction   :: Float
+    { imgSize         :: Int
+    , bgColor         :: PixelRGBA8
+    , segColor        :: PixelRGBA8
+    , segPixelWidth   :: Float
+    , padFraction     :: Float
+    , traceColor      :: PixelRGBA8
+    , tracePixelWidth :: Maybe Float
+    , traceSubframes  :: Maybe Int
     }
 
 
